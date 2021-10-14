7cdf6db8738d121b022485a0122e328d817448c9
Denis <48333782+dkanukov@users.noreply.github.com>
2021-10-14 20:35:39 +0300

    add scroll speed + stop

--- js/index.js
+++ js/index.js
@@ -7,34 +7,41 @@
     let btnStart = document.getElementById('start');
     let btnSpeed = document.getElementById('speed')
     let btnStop = document.getElementById('stop');
-    let play = true;
+    let play;
+    let speed = 50;
+    let move = 0;
 
     btnSession.addEventListener('click', function (){
         if (contentInput.value){
             modal.classList.add('modal-show')
             promptContainer.innerHTML = contentInput.value.replace(/\n/g, '<br/>');
-
-            btnExit.addEventListener('click', function (){
-               modal.classList.remove('modal-show');
-            });
-
-            btnStart.addEventListener('click', function (){
-                let move = 0;
-                function scroll() {
-                    promptContainer.style.transform = 'translateY(' + move + 'px)';
-                    move--;
-                }
-                setInterval(scroll, 50);
-
-            });
-
-
-
-
         }
         else{
             alert("вы ничего не читаете :(");
         }
 
     });
+
+    btnStart.addEventListener('click', function (){
+            let timeId = setInterval(scroll, speed);
+            btnSpeed.addEventListener('change', function (){
+                speed = 100 - btnSpeed.value;
+                clearInterval(timeId);
+                timeId = setInterval(scroll, speed);
+            });
+            btnStop.addEventListener('click', function (){
+               console.log('stop');
+               clearInterval(timeId);
+            });
+            function scroll() {
+                promptContainer.style.transform = 'translateY(' + move + 'px)';
+                move--;
+            }
+    });
+
+    btnExit.addEventListener('click', function (){
+        modal.classList.remove('modal-show');
+    });
+
+
 })();
