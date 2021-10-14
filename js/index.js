d8efebe871624d469d22342c01f9c5cc49ec01e5
Denis <48333782+dkanukov@users.noreply.github.com>
2021-10-14 18:25:18 +0300

    add scroll func

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 (function (){
     let modal = document.getElementById('modal');
-    let btnStart = document.getElementById('btn-start');
+    let btnSession = document.getElementById('btn-start');
     let contentInput = document.getElementById('content-input');
     let promptContainer = document.getElementById('prompter-container');
     let btnExit = document.getElementById('exit');
+    let btnStart = document.getElementById('start');
+    let btnSpeed = document.getElementById('speed')
+    let btnStop = document.getElementById('stop');
+    let play = true;
 
-    btnStart.addEventListener('click', function (){
+    btnSession.addEventListener('click', function (){
         if (contentInput.value){
             modal.classList.add('modal-show')
             promptContainer.innerHTML = contentInput.value.replace(/\n/g, '<br/>');
@@ -13,6 +17,18 @@
             btnExit.addEventListener('click', function (){
                modal.classList.remove('modal-show');
             });
+
+            btnStart.addEventListener('click', function (){
+                let move = 0;
+                function scroll() {
+                    promptContainer.style.transform = 'translateY(' + move + 'px)';
+                    move--;
+                }
+                setInterval(scroll, 50);
+
+            });
+
+
 
 
         }
